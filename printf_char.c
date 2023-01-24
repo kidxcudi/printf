27afdf1640a417a73a6d03192f241afbaf4b8b0c
kidxcudi <112644403+kidxcudi@users.noreply.github.com>
2023-01-24 21:48:18 +0300

    Update printf_char.c

--- printf_char.c
+++ printf_char.c
@@ -1,1 +1,15 @@
-dfhsd
+#include "main.h"
+
+/**
+ * printf_char - prints a char.
+ * @val: arguments.
+ * Return: 1.
+ */
+int printf_char(va_list val)
+{
+	char s;
+
+	s = va_arg(val, int);
+	_putchar(s);
+	return (1);
+}
